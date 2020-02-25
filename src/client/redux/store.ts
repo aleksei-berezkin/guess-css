85da33b263e5afdc947b960d42b783c80ba717ab
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2020-02-25 11:45:10 +0300

    Cleanup

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -3,11 +3,12 @@ import { Action, DisplayChoice, DisplayLayout, Type } from './actions';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { rootSaga } from './saga';
+import * as R from 'ramda';
 
 
 export interface State {
     puzzler: GenPuzzlerResponse | null,
-    choices: ChoiceFormatted[],
+    choices: (ChoiceFormatted | null)[],
 }
 
 const initialState: State = {
@@ -23,16 +24,26 @@ const rootReducer = combineReducers({
         return puzzler;
     },
 
-    choices: function(choices: ChoiceFormatted[] = initialState.choices, action: Action) {
+    choices: function(choices: (ChoiceFormatted | null)[] = initialState.choices, action: Action) {
+        if (action.type === Type.DISPLAY_LAYOUT) {
+            return trimOrExtend(choices, (action as DisplayLayout).puzzler.choicesCount);
+        }
+
         if (action.type === Type.DISPLAY_CHOICE) {
-            const a = action as DisplayChoice;
-            const newChoices = [...choices];
-            newChoices[a.choice] = a.code;
-            return newChoices;
+            const {choice, code} = action as DisplayChoice;
+            return [...choices.slice(0, choice), code, ...choices.slice(choice + 1)];
         }
         return choices;
     },
 });
+
+function trimOrExtend<T>(a: (T | null)[], size: number): (T | null)[] {
+    if (a.length >= size) {
+        return a.slice(0, size);
+    }
+
+    return [...a, ...R.repeat(null, size - a.length)];
+}
 
 export function createAppStore() {
     const sagaMiddleware = createSagaMiddleware();
