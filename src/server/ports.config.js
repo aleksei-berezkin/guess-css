76155e56da181c857216a9e49d844f6b807b5632
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2020-03-15 00:47:33 +0300

    Port config

--- src/server/ports.config.js
+++ src/server/ports.config.js
@@ -1,3 +1,2 @@
 exports.WEB_DEV_PORT = 3000;
 exports.API_DEV_PORT = 3001;
-exports.PROD_PORT = 80;
--- src/server/runProdServer.ts
+++ src/server/runProdServer.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import addApi from './serverApi';
 import { sendRenderedApp } from './renderServerSide';
 // @ts-ignore
-import { WEB_DEV_PORT, PROD_PORT } from './ports.config';
+import { WEB_DEV_PORT } from './ports.config';
 
 if (process.env.NODE_ENV === 'development') {
     console.warn('WARNING: Production runs in dev mode');
@@ -20,5 +20,5 @@ app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 
 addApi(app);
 
-const port = process.env.NODE_ENV === 'production' ? PROD_PORT : WEB_DEV_PORT;
+const port = process.env.PORT || WEB_DEV_PORT;
 app.listen(port, () => console.log(`Prod server is listening on port ${port}`));
