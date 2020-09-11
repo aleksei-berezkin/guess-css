06021dd905e5e8828d7fda840ab89b13f1f6506d
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2020-09-11 23:34:48 +0300

    Up TypeScript

--- src/client/redux/preloadedStateId.ts
+++ src/client/redux/preloadedStateId.ts
@@ -4,6 +4,6 @@ export const PRELOADED_STATE_ID = '__PRELOADED_STATE__';
 
 declare global {
     interface Window {
-        [PRELOADED_STATE_ID]: State;
+        [PRELOADED_STATE_ID]?: State;
     }
 }
