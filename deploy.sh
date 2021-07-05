f50c6b04c43b0cc7a561b335febf854f35615765
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2021-07-05 19:30:10 +0300

    Fix CNAME in deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ rm -rf ../guess-css-site/docs
 mkdir ../guess-css-site/docs
 cp dist/* ../guess-css-site/docs/
 touch ../guess-css-site/docs/.nojekyll
+touch ../guess-css-site/docs/CNAME
+echo 'www.guess-css.app' > ../guess-css-site/docs/CNAME
 cd ../guess-css-site
 git add .
 git commit -m Site
